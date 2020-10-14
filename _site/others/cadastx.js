ba2b3c421adb899603363306c7fb78f6b2160711
Taw-ham Balbino <taw-hambalbino@MacBook-Air-de-Taw-ham.local>
2020-10-14 20:24:02 -0300

    FINISH

---
+++ _site/others/cadastx.js
@@ -0,0 +1,124 @@
+class Aluno {
+    constructor(email, nome, senha) {
+        this.email = email;
+        this.nome = nome;
+        this.senha = senha;
+    }
+}
+class Aluno_Service {
+    constructor(url) {
+        this.url = url;
+    }
+    inserir(email, nome, senha) {
+        return fetch(this.url, {
+            method: "POST",
+            body: JSON.stringify(email, nome, senha),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resposta => resposta.json())
+    }
+    listar() {
+        return fetch(this.url, {
+            method: "GET"
+        }).then(resposta => resposta.json())
+    }
+}
+class PROFESSOR {
+    constructor(nome, senha, email) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
+}
+class ProfessorService {
+    constructor(url) {
+        this.url = url
+    }
+    inserir(nome, senha, email) {
+        return fetch(this.url, {
+            method: "POST",
+            body: JSON.stringify(nome, senha, email),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(
+            resposta => resposta.json()
+        );
+    }
+    listar(){
+        return fetch(this.url,{
+            method: "GET"
+        }).then(
+            resposta => resposta.json()
+
+        )
+    }
+}
+
+let tipo_loginx;
+
+const tipo_userx = document.getElementsByName("tipo_login");
+for (let i = 0; i <= tipo_userx.length - 1; i++) {
+    tipo_userx[i].onclick = function () {
+        tipo_loginx = event.target.id;
+        console.log(tipo_loginx);
+        if (tipo_loginx === "bbx2") {
+            $("#logx2").css('background', 'linear-gradient(lightblue,whitesmoke,whitesmoke)');
+            $("#escolhaxa").css('background', 'lightblue');
+            $("#obx02").hide("fast");
+            $("#bbx2").css('background-color','blue');
+            $("#ccx2").css('background-color','lightslategray');
+            $("#wallx2").css('padding-bottom', '3%');
+        } else if (tipo_loginx === "ccx2") {
+            $("#logx2").css('background', 'linear-gradient(lightgreen,whitesmoke,whitesmoke)');
+            $("#escolhaxa").css('background', 'lightgreen');
+            $("#obx02").hide("fast");
+            $("#ccx2").css('background-color','green');
+            $("#bbx2").css('background-color','lightslategray');
+            $("#wallx2").css('padding-bottom', '3%');
+        }
+    }
+}
+document.getElementById("fazer_cadastro").onclick = function () {
+    const nome = document.getElementById("nome_cadastro").value;
+    const senha = document.getElementById("senha_cadastro").value;
+    const email = document.getElementById("email_cadastro").value;
+    if (nome != "" && senha != "" && email != "") {
+        if (tipo_loginx === "bbx2") {
+            const aluno_service = new Aluno_Service(`https://yprto.sse.codesandbox.io/Alunos?email=${email}`)
+            aluno_service.listar().then(resposta => {
+                if (resposta.length > 0) {
+                    swal('Cadastro Inválido!', '- e-mail existente em outra conta -', 'error')
+                } else {
+                    const aluno = new Aluno(email, nome, senha);
+                    const aluno_service2 = new Aluno_Service("https://yprto.sse.codesandbox.io/Alunos");
+                    aluno_service2.inserir(aluno).then(resposta => {
+                        window.location.assign("./others/logx.html");
+                    })
+                }
+            })
+
+        } else if (tipo_loginx === "ccx2") {
+            const professor_service = new ProfessorService(`https://yprto.sse.codesandbox.io/professores?email=${email}`);
+            professor_service.listar().then(resposta => {
+                if (resposta.length > 0) {
+                    swal('Cadastro Inválido!', '- e-mail existente em outra conta -', 'error')
+
+                } else {
+                    const professor_service = new ProfessorService("https://yprto.sse.codesandbox.io/professores");
+                    const professor = new PROFESSOR(nome, senha, email);
+                    professor_service.inserir(professor).then(resposta => {
+                        window.location.assign("./others/logx.html");
+                    })
+
+                }
+            })
+        }
+        if (tipo_loginx == "") {
+            swal('Cadastro Inválido!', '- escolha um tipo de usuário -', 'error');
+        }
+    } else {
+        swal('Cadastro Inválido!', '- preencha todos os campos -', 'error');
+    }
+}
---
+++ _site/others/loginx.js
@@ -0,0 +1,114 @@
+class Aluno {
+    constructor(email, nome, senha) {
+        this.email = email;
+        this.nome = nome;
+        this.senha = senha;
+    }
+}
+class Aluno_Service {
+    constructor(url) {
+        this.url = url;
+    }
+    inserir(nome, senha) {
+        return fetch(this.url, {
+            method: "POST",
+            body: JSON.stringify(nome, senha),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resposta => resposta.json())
+    }
+    listar() {
+        return fetch(this.url, {
+            method: "GET"
+        }).then(resposta => resposta.json())
+    }
+}
+class PROFESSOR {
+    constructor(nome, senha, email) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
+}
+class ProfessorService {
+    constructor(url) {
+        this.url = url
+    }
+    listar() {
+        return fetch(this.url, {
+            method: "GET"
+        }).then(resposta => resposta.json())
+    }
+    inserir(nome, senha) {
+        return fetch(this.url, {
+            method: "POST",
+            body: JSON.stringify(nome, senha),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resposta => resposta.json())
+    }
+}
+
+let tipo_loginx;
+
+let tipo_userx = document.getElementsByTagName("button");
+for (let i = 0; i <= tipo_userx.length - 1; i++) {
+    tipo_userx[i].onclick = function () {
+        tipo_loginx = event.target.id
+        if (tipo_loginx == "bbx") {
+            $("#logx").css('background', 'linear-gradient(whitesmoke,lightblue)');
+            $("#obx").hide("fast");
+            $("#tipologinx").css('background', 'lightblue');
+            $("#bbx").css('background-color','blue');
+            $("#ccx").css('background-color','lightslategray');
+            $("#wallx").css('padding-bottom', '7.2%');
+        } else {
+            $("#logx").css('background', 'linear-gradient(whitesmoke,lightgreen');
+            $("#obx").hide("fast");
+            $("#tipologinx").css('background', 'lightgreen');
+            $("#ccx").css('background-color','green');
+            $("#bbx").css('background-color','lightslategray');
+            $("#wallx").css('padding-bottom', '7.2%');
+        }
+    }
+}
+document.getElementById("fazer_login").onclick = function () {
+    const usuario = $("#nome_login").val();
+    const senha = $("#senha_login").val();
+    if (tipo_loginx == "bbx") {
+        const aluno_service = new Aluno_Service(`https://yprto.sse.codesandbox.io/Alunos?email=${usuario}&senha=${senha}`);
+        aluno_service.listar().then(resposta => {
+            if (resposta.length === 0) {
+                swal('Login Inválido!', '- email ou senha incorretos -', 'error');
+            } else {
+                console.log(resposta);
+                const id_login = resposta[0].id;
+                const nome_login = resposta[0].nome;
+                localStorage.setItem('id', id_login);
+                localStorage.setItem('e-mail', usuario);
+                localStorage.setItem('nome', nome_login);
+                window.location.assign("./others/pagina_aluno.html");
+            }
+        })
+    } else if (tipo_loginx === "ccx") {
+        let loginservice = new ProfessorService(`https://yprto.sse.codesandbox.io/professores?email=${usuario}&senha=${senha}`);
+        loginservice.listar().then(results => {
+            if (results.length === 0) {
+                swal('Login Inválido!', '- email ou senha incorretos -', 'error');
+            } else {
+                console.log(results);
+                const id = results[0].id;
+                const email = results[0].email;
+                const nome_user = results[0].nome;
+                localStorage.setItem('id', id);
+                localStorage.setItem('email', email);
+                localStorage.setItem('nome', nome_user);
+                window.location.assign("./others/professor_pagina.html")
+            }
+        })
+    } else {
+        swal('Login Inválido!', '- escolha seu tipo de usuário -', 'error');
+    }
+}
