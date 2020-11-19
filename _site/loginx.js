7b2e598c9854a75bfd4454ff1cff22b1887579fd
jhonncamarg0 <jhonatha.emmanuel@academico.ifpb.edu.br>
2020-11-18 22:44:39 -0300

    fix

--- _site/loginx.js
+++ _site/loginx.js
@@ -64,14 +64,14 @@
                 $("#tipologinx").css('background-color', 'lightblue');
                 $("#bbx").css('background-color', 'blue');
                 $("#ccx").css('background-color', 'lightslategray');
-                $("#wallx").css('padding-bottom', '6.6%');
+                $("#wallx").css('padding-bottom', '6.5%');
             } else {
                 $("#logx").css('background', 'linear-gradient(whitesmoke,lightgreen');
                 $("#obx").hide("fast");
                 $("#tipologinx").css('background-color', 'lightgreen');
                 $("#ccx").css('background-color', 'green');
                 $("#bbx").css('background-color', 'lightslategray');
-                $("#wallx").css('padding-bottom', '6.6%');
+                $("#wallx").css('padding-bottom', '6.5%');
             }
         }
     }
